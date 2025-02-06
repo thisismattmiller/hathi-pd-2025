9bd13d15cb58e0cf246c119a6c65ef29495c2625
Matthew Miller <thisismattmiller@gmail.com>
2025-02-05 21:55:36 -0500

    typo

--- scripts/build_stats.py
+++ scripts/build_stats.py
@@ -6,6 +6,12 @@ import string
 
 works_count = {}
 top_titles = {}
+has_lcc = 0
+got_lcc = 0
+
+has_lcsh = 0
+got_lcsh = 0
+
 count=0
 with gzip.open('../data/hathi_collection_final_dataset.ndjson.gz','rt') as f:
 
@@ -20,6 +26,20 @@ with gzip.open('../data/hathi_collection_final_dataset.ndjson.gz','rt') as f:
 
         works_count[data['genre_format']]=works_count[data['genre_format']]+1
 
+        counted_lcsh = False
+        for field in data['marc']['fields']:
+            if '050' in field:
+                has_lcc=has_lcc+1
+            if '650' in field and counted_lcsh == False:
+                has_lcsh=has_lcsh+1
+                counted_lcsh=True
+
+        if data['lcc'] != None and data['lcc'] != False:
+            got_lcc = got_lcc+1
+
+        if 'oclc_data' in data:
+            got_lcsh=got_lcsh+1
+
 
 print(count)
 print(works_count)
@@ -27,3 +47,14 @@ print(works_count)
 for k in works_count:
 
     print(k, works_count[k] / count * 100)
+
+
+print("has_lcc",has_lcc, has_lcc/count * 100)
+print("got_lcc",got_lcc, got_lcc/count * 100)
+
+print("has_lcsh",has_lcsh, has_lcsh/count * 100)
+print("got_lcsh",got_lcsh, got_lcsh/count * 100)
+
+
+
+
